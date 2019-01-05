32f5b89930e3b4c72c6abc72c44d74b06c6bde43
Cassidy Pignatello <cassidy.pignatello@gmail.com>
2019-01-05 16:12:31 +0800

    dispatches changes to the store

--- index.js
+++ index.js
@@ -30,6 +30,7 @@
   id: 0
 }
 
+// Reducer function
 function todos (state = [], action) {
   if (action.type === 'ADD_TODO') {
     return state.concat([action.todo])
@@ -37,17 +38,17 @@ function todos (state = [], action) {
   return state
 }
 
-function createStore() {
+function createStore(reducer) {
   // The store should have four parts
   // 1. the state
-  // 2. get the state
-  // 3. listen to changes on the state
-  // 4. update the state
+  // 2. get the state (getState)
+  // 3. listen to changes on the state (subscribe)
+  // 4. update the state (dispatch)
 
   let state
   let listeners = []
 
-  const getState = () => getState
+  const getState = () => state
 
   const subscribe = (listener) => {
     listeners.push(listener)
@@ -56,10 +57,24 @@ function createStore() {
     }
   }
 
-  const update
+  const dispatch = (action) => {
+    state = reducer(state, action)
+    listeners.forEach((listener) => listener())
+  }
 
   return {
     getState,
     subscribe,
+    dispatch
   }
 }
+
+const store = createStore(todos)
+store.dispatch({
+  type: 'ADD_TODO',
+  todo: {
+    id: 0,
+    name: 'Learn Redux',
+    complete: false
+  }
+})
