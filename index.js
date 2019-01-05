caa2fee40d70456eee76d5f51354b0d53dd4fe8d
Cassidy Pignatello <cassidy.pignatello@gmail.com>
2019-01-05 15:46:17 +0800

    creates reducer for add todos

--- index.js
+++ index.js
@@ -30,6 +30,13 @@
   id: 0
 }
 
+function todos (state = [], action) {
+  if (action.type === 'ADD_TODO') {
+    return state.concat([action.todo])
+  }
+  return state
+}
+
 function createStore() {
   // The store should have four parts
   // 1. the state
